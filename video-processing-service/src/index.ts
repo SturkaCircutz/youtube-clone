a01b96a604a66de453b0ee8efc16e997d4b80fb2
SturkaCircutz <313721325sjw@gmail.com>
2026-08-24 19:17:25 -0600

    new

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -2,16 +2,36 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg";
 
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res)=> {
+app.use(express.json());
+
+app.post("/process-video", (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("bad request: missing file path.");
+        return;
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            return res.status(200).send("processing finished successfully");
+        })
+        .on("error", (err) => {
+            console.log(`an error occurred: ${err.message}`);
+            return res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
+});
+
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Video processing service at http://localhost:${port}`);
 });
-
-
