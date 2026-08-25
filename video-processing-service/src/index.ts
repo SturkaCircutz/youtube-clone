f9b28857ac1e644aaf006b4569c41b55ac832faa
SturkaCircutz <313721325sjw@gmail.com>
2026-08-24 19:04:31 -0600

    new

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,17 @@
+//entry point
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res)=> {
+    res.send("Hello World!");
+});
+
+app.listen(port, ()=>{
+    console.log(`Video processing service at http://localhost:${port}`);
+});
+
+
